71d2cc1f66e180b322921a78dbf72292ebea3515
mateconpizza <81921095+mateconpizza@users.noreply.github.com>
2025-11-10 10:48:23 -0300

    feat: add `utils`

---
+++ lua/winbar/util.lua
@@ -0,0 +1,67 @@
+---@diagnostic disable: undefined-field
+---@class WinBar.Utils
+local M = {}
+
+-- get the relative path of a buffer name, falling back to filename only for home directory paths.
+---@param bufname string
+---@return string
+function M.get_relative_path(bufname)
+  local relative_path = vim.fn.fnamemodify(bufname, ':~:.')
+  if relative_path:sub(1, 1) == '~' then
+    relative_path = vim.fn.fnamemodify(bufname, ':t')
+  end
+
+  return relative_path
+end
+
+-- check if a buffer is visible in any non-floating window.
+---@param bufnr integer Buffer handle
+---@return boolean true if visible in at least one normal window, false otherwise
+function M.is_visible_in_normal_win(bufnr)
+  return vim.iter(vim.fn.win_findbuf(bufnr)):any(function(win)
+    return not M.is_float(win)
+  end)
+end
+
+-- check if the window is float
+---@param winid integer
+---@return boolean
+function M.is_float(winid)
+  return vim.api.nvim_win_get_config(winid).relative ~= ''
+end
+
+-- create a autocommand group
+---@param name string
+function M.augroup(name)
+  return vim.api.nvim_create_augroup('me_' .. name, { clear = true })
+end
+
+-- sets a highlight group
+function M.set_hl(name, val)
+  vim.api.nvim_set_hl(0, name, val)
+end
+
+-- check if current buffer is a special buffer type or filetype that should be excluded.
+---@param c WinBar.Config
+function M.is_special_buffer(c)
+  local buftype = vim.bo.buftype
+  local filetype = vim.bo.filetype
+
+  -- skip special buffer types
+  for _, bt in ipairs(c.exclude_buftypes) do
+    if buftype == bt then
+      return true
+    end
+  end
+
+  -- skip special filetypes
+  for _, ft in ipairs(c.exclude_filetypes) do
+    if filetype == ft then
+      return true
+    end
+  end
+
+  return false
+end
+
+return M
