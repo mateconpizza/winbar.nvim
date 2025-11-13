4f71a77835b9a5d212f27ff01bf41c7b3586e93b
mateconpizza <81921095+mateconpizza@users.noreply.github.com>
2025-11-12 21:22:28 -0300

    feat: update `config`

--- lua/winbar/config.lua
+++ lua/winbar/config.lua
@@ -1,51 +1,73 @@
 ---@class winbar.diagnosticIcons
----@field error? string icon for errors.
----@field hint? string icon for hints.
----@field info? string icon for infos.
----@field warning? string icon for warnings.
+---@field error string? icon for errors.
+---@field hint string? icon for hints.
+---@field info string? icon for infos.
+---@field warning string? icon for warnings.
 
 ---@class winbar.diagnostic
----@field enabled? boolean enable diagnostics.
----@field style? string diagnostics style (minimalist or standard).
----@field bug_icon? string show bug icon.
----@field show_detail? boolean show detail.
----@field icons? winbar.diagnosticIcons
+---@field enabled boolean? enable diagnostics.
+---@field style string? diagnostics style (minimalist or standard).
+---@field bug_icon string? show bug icon.
+---@field show_detail boolean? show detail.
+---@field icons winbar.diagnosticIcons?
 
 ---@class winbar.lspStatus
----@field enabled? boolean enable LSP client name display.
----@field separator? string separator between multiple LSP clients.
----@field format? fun(clients: string): string custom formatter for client names.
+---@field enabled boolean? enable LSP client name display.
+---@field separator? string? separator between multiple LSP clients.
+---@field format fun(clients: string): string? custom formatter for client names.
 
 ---@class winbar.icons
----@field modified? string icon for modified buffers.
----@field readonly? string icon for readonly buffers.
----@field git_branch? string icon for Git branch indicator.
+---@field modified string? icon for modified buffers.
+---@field readonly string? icon for readonly buffers.
 
 ---@class winbar.Layout
----@field left? string[] ordered list of left-aligned component names.
----@field right? string[] ordered list of right-aligned component names.
+---@field left string[]? ordered list of left-aligned component names.
+---@field right string[]? ordered list of right-aligned component names.
 
 ---@class winbar.userHighlights
----@field git_branch? winbar.highlightAttrs git branch highlights.
----@field lsp_status? winbar.highlightAttrs LSP client name highlights.
+---@field winbar winbar.highlightAttrs? active window WinBar highlight
+---@field winbarnc winbar.highlightAttrs? inactive window WinBar highlight
+---@field lsp_status winbar.highlightAttrs? LSP client name highlights.
+---@field readonly winbar.highlightAttrs? read-only indicator highlight
+---@field modified winbar.highlightAttrs? modified buffer indicator highlight
+---@field git_branch winbar.highlightAttrs? git branch highlights.
+---@field diffadded winbar.highlightAttrs? git diff added lines highlight
+---@field diffchanged winbar.highlightAttrs? git diff changed lines highlight
+---@field diffremoved winbar.highlightAttrs? git diff removed lines highlight
 
 ---@class winbar.exclusions
----@field filetypes? string[] filetypes to exclude from WinBar display.
----@field buftypes? string[] buffer types to exclude from WinBar display.
+---@field filetypes string[]? filetypes to exclude from WinBar display.
+---@field buftypes string[]? buffer types to exclude from WinBar display.
+
+---@class winbar.gitbranch
+---@field enabled boolean?
+---@field icon string? icon for Git branch indicator.
+
+---@class winbar.gitdiff
+---@field enabled boolean?
+---@field added string? icon for added files in git diff
+---@field changed string? icon for changed files in git diff
+---@field removed string? icon for removed files in git diff
+
+---@class winbar.git
+---@field branch winbar.gitbranch? git branch configuration
+---@field diff winbar.gitdiff? git diff configuration
 
 ---@class (exact) winbar.config
----@field enabled? boolean
----@field file_icon? boolean show file icon.
----@field diagnostics? winbar.diagnostic diagnostics.
----@field lsp? winbar.lspStatus LSP client name display..
----@field icons? winbar.icons icons used throughout the WinBar.
----@field show_single_buffer? boolean show with single buffer.
----@field exclusions? table<string, string[]>
----@field git_branch? boolean show git branch.
----@field styles? table<string, winbar.highlightAttrs> winbar highlights.
+---@field enabled boolean?
+---@field update_interval integer? interval in milliseconds
+---@field file_icon boolean? show file icon.
+---@field diagnostics winbar.diagnostic? diagnostics.
+---@field lsp winbar.lspStatus? LSP client name display..
+---@field icons winbar.icons? icons used throughout the WinBar.
+---@field show_single_buffer boolean? show with single buffer.
+---@field exclusions table<string, string[]>?
+---@field git winbar.git?
+---@field styles winbar.userHighlights? winbar highlights.
 return {
   -- Core behavior
   enabled = true, -- Enable the WinBar plugin
+  update_interval = 200, -- How much to wait in milliseconds before update (git diff, diagnostics)
   file_icon = true, -- Show file icon (e.g., via nvim-web-devicons)
   show_single_buffer = true, -- Show WinBar even with a single visible buffer
   exclusions = {
@@ -72,7 +94,6 @@ return {
   icons = {
     modified = '[+]', -- Shown for unsaved buffers (choice: ●)
     readonly = '[RO]', -- Shown for readonly buffers (choice: )
-    git_branch = '', -- Git branch icon (choice: )
   },
   -- Diagnostics configuration
   diagnostics = {
@@ -95,11 +116,22 @@ return {
       return clients
     end,
   },
-  -- Git branch display
-  git_branch = true, -- Show the current Git branch
+  -- Git display
+  git = {
+    branch = {
+      enabled = true,
+      icon = '', -- Git branch icon (choice: )
+    },
+    diff = {
+      enabled = true,
+      added = '+',
+      changed = '~',
+      removed = '-',
+    },
+  },
   -- Layout of the WinBar
   layout = {
-    left = { 'git_branch' }, -- Components aligned to the left
+    left = { 'git_branch', 'git_diff' }, -- Components aligned to the left
     right = { -- Components aligned to the right
       'lsp_status',
       'diagnostics',
