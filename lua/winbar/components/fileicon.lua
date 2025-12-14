0378cf5e189e410eba45fededa117970016c6b15
mateconpizza <81921095+mateconpizza@users.noreply.github.com>
2025-12-14 19:17:42 -0300

    fix(comp/fileicon): resolve file icon lookup with `mini.icons`

--- lua/winbar/components/fileicon.lua
+++ lua/winbar/components/fileicon.lua
@@ -4,6 +4,10 @@ local function cache()
   return require('winbar.cache')
 end
 
+local function highlighter()
+  return require('winbar.highlight')
+end
+
 ---@class winbar.components.fileicon: winbar.component
 local M = {}
 
@@ -11,25 +15,39 @@ local M = {}
 local function get_icon(bufnr)
   local filename = vim.fn.fnamemodify(vim.api.nvim_buf_get_name(bufnr), ':t')
   local filetype = vim.bo[bufnr].filetype
+  local ext = vim.fn.fnamemodify(filename, ':e')
   local icon, hl
 
   -- try mini.icons
-  local ok_mini, mini = pcall(require, 'mini.icons')
-  if ok_mini then
-    icon, hl = mini.get('filetype', filetype)
+  local _, mini_icons = pcall(require, 'mini.icons')
+  if _G.MiniIcons then
+    local is_default
+
+    -- by filetype
+    icon, hl, is_default = mini_icons.get('filetype', filetype)
+    if not is_default then return highlighter().string(hl, icon) end
+
+    -- by extension
+    if ext ~= '' then
+      icon, hl, is_default = mini_icons.get('extension', ext)
+      if not is_default then return highlighter().string(hl, icon) end
+    end
+
+    -- by file (lets `mini.icons` do its full internal resolution)
+    icon, hl = mini_icons.get('file', filename)
+    return highlighter().string(hl, icon)
   end
 
   -- fallback to devicons
   if not icon then
     local ok_dev, devicons = pcall(require, 'nvim-web-devicons')
     if ok_dev then
-      local ext = vim.fn.fnamemodify(filename, ':e')
       if ext == '' then ext = filetype end
       icon, hl = devicons.get_icon(filename, ext or filetype, { default = true })
     end
   end
 
-  return (icon and hl) and ('%#' .. hl .. '#' .. icon .. '%*') or ''
+  return (icon and hl) and (highlighter().string(hl, icon)) or ''
 end
 
 M.name = 'fileicon'
